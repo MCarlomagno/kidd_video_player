84d67ac0bc7b2dc6b54fa73ed7280f41ea4e3984
mcarlomagno <marcoscarlomagno2@gmail.com>
2020-06-21 16:46:39 -0300

    doc: added comments to classes

--- lib/models/layout_configs.dart
+++ lib/models/layout_configs.dart
@@ -1,21 +1,61 @@
-
-
-
 import 'package:flutter/material.dart';
+
+/// #### Inmutable class for UI customizations
+/// 
+/// It recieves 11 parameters 
+/// All of them can be null and have values by default
+/// 
+/// see also: 
+/// https://github.com/MCarlomagno/kidd_video_player
 
 @immutable
 class LayoutConfigs {
 
+  /// Shows (or not) the slider  for volume control 
+  /// and icon button for set the volume on mute.
+  /// (by default [showVolumeControl = true])
   final bool showVolumeControl;
+
+  /// Shows (or not) the slider for video progress control
+  /// and the duration and progress time.
+  /// (by default [showVideoControl = true])
   final bool showVideoControl;
+
+  /// Shows (or not) the full screen button.
+  /// (by default [showFullScreenButton = true])
   final bool showFullScreenButton;
+
+  /// Sets (or not) the video looping 
+  /// (by default [inLoop = true])
   final bool inLoop;
+
+  /// Sets the play, pause, volume, full screen and video duration colors
+  /// (by default [iconsColor = Colors.white])
   final Color iconsColor;
+
+  /// Sets the slider primary color
+  /// (by default [sliderColor = Colors.white])
   final Color sliderColor;
+
+  /// Sets the slider background color
+  /// (by default [backgroundSliderColor = Colors.grey])
   final Color backgroundSliderColor;
+
+  /// Sets the color of the empty spaces between the video frame and the widget border
+  /// (by default [backgroundColor = Colors.black])
   final Color backgroundColor;
+
+  
+  /// Sets the color of the [CircularProgressIndicator] when the video is loading
+  /// (by default [loaderColor = Colors.white])
   final Color loaderColor;
+
+  /// Sets the icon to pause the video
+  /// (by default [pauseIcon = Icons.pause])
   final IconData pauseIcon;
+
+  /// Sets the icon to play the video
+  /// (by default [playIcon = Icons.play_arrow])
   final IconData playIcon;
 
   const LayoutConfigs({
@@ -32,6 +72,7 @@ class LayoutConfigs {
     this.playIcon = Icons.play_arrow
   });
 
+  /// By defaul object (used when the values are not customized).
   static const byDefault = LayoutConfigs();
  
 }
--- lib/video_controller_service/video_controller_service.dart
+++ lib/video_controller_service/video_controller_service.dart
@@ -1,58 +1,77 @@
 import 'dart:async';
 import 'package:video_player/video_player.dart';
 
+/// #### Singleton service for video player controller handling
+/// 
+/// The propose of this service is to add a new level of abstraction to manage the
+/// video controller and make it more ease to access from different widgets.
+/// 
+/// see also: 
+/// https://github.com/MCarlomagno/kidd_video_player
+
 class VideoControllerService {
 
+  /// Singleton initialization.
   static final VideoControllerService _videoControllerService = VideoControllerService._internal();
   VideoControllerService._internal();
 
+  /// Factory pattern for constrictor.
   factory VideoControllerService() {
     return _videoControllerService;
   }
 
-
+  /// controller that will handle video player actions.
   VideoPlayerController _controller;
   VideoPlayerController get controller => this._controller;
 
-  // stream that listen the controller.position value
+  /// stream that listen the [controller.position] value
   Stream<Duration> get streamToProgress => this._controller.position.asStream();
 
-  // stream that listen the controller value
+  /// stream that listen the [controller] buffered value.
   List<DurationRange> get buffered => this._controller.value.buffered;
 
+  /// stream that listen the [controller] value.
   VideoPlayerValue get value => _controller.value;
 
+  /// Setter for [_controller].
   void setController(VideoPlayerController controller) {
     this._controller = controller;
   }
 
+  /// Initializes the controller and sets the looping with the given boolean value.
   Future<void> initializeController({bool inLoop}) async {
     await this._controller.initialize();
     // Use the controller to loop the video.
     await _controller.setLooping(inLoop);
-  }
+  } 
 
+  /// Plays the video.
   Future<void> playVideo() {
     _controller.play();
   }
 
+  /// Pauses the video.
   Future<void> pauseVideo() {
     _controller.pause();
   }
 
+  /// Moves the video progress to the given Duration value
   Future<void> seekTo(Duration value) {
     this._controller.seekTo(value);
   }
 
+  /// Sets the loop boolean variable for restart the video when it finishes.
   void setLooping(bool value) {
     _controller.setLooping(value);
   }
 
+  /// Sets the volume value (between 0.0 and 1.0).
   void setVolume(double value) {
     this._controller.setVolume(value);
   }
 
   void dispose() {
+    // disposes the controller.
     this._controller.dispose();
   }
 }
