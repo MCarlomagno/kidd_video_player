aa2e3c12f1ede1ab9b1e1723374cc20adb9c95bb
mcarlomagno <marcoscarlomagno2@gmail.com>
2020-06-23 08:50:22 -0300

    fix: flutter format to files, return values solved

--- lib/video_player/full_screen.dart
+++ lib/video_player/full_screen.dart
@@ -2,31 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 /// #### Widget for show the video in full screen
-/// 
+///
 /// Class navigated from original video container to display the content in full screen.
-/// 
-/// It recieves 2 parameters 
+///
+/// It recieves 2 parameters
 /// [backgroundColor] [child].
 /// assertion [child] != null.
-/// 
-/// see also: 
+///
+/// see also:
 /// https://github.com/MCarlomagno/kidd_video_player
 class KiddFullScreen extends StatefulWidget {
-
   /// Customizable color for the empty space around the video
   final Color backgroundColor;
 
   /// Widget that contains all the video UI content (included [Layout])
   final Widget child;
 
-  const KiddFullScreen({Key key, @required this.child, this.backgroundColor = Colors.black}) : assert(child != null), super(key: key);
+  const KiddFullScreen(
+      {Key key, @required this.child, this.backgroundColor = Colors.black})
+      : assert(child != null),
+        super(key: key);
 
   @override
   _KiddFullScreenState createState() => _KiddFullScreenState();
 }
 
 class _KiddFullScreenState extends State<KiddFullScreen> {
-
   @override
   void dispose() {
     // resets the device orientation before close the full screen
